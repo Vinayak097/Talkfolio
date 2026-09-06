ed52d12bcda02518a4f1bd06c6cf80ca60667b9d
unknown <vinayak20injamure@gmail.com>
2026-09-07 00:00:13 +0530

    frontend initilizatins

---
+++ frontend/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import Searchbar from "./component/Searchbar";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <div>
+        <Searchbar></Searchbar>
+      </div>
+    </div>
+  );
+}
